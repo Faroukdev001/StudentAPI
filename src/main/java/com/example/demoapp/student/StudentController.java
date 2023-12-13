f5e8a5fcce66cf11f44bb6164dd36f69f54e578a
Al-Farouk <faroukarogundade@gmail.com>
2023-12-13 09:15:19 +0100

    first commit

---
+++ src/main/java/com/example/demoapp/student/StudentController.java
@@ -0,0 +1,62 @@
+package com.example.demoapp.student;
+
+import java.util.List;
+
+import com.example.demoapp.student.Student;
+import com.example.demoapp.student.StudentService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    // Constructor
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
